f15dfca1bb65dedae1eff1d69fd93ec56e82eb3a
brookefitzgerald <brooke.anne.fitzgerald@gmail.com>
2017-11-06 19:02:14 -0500

    updated model to be more in line with fact/dimension

--- neural_exploration/visualize/models.py
+++ neural_exploration/visualize/models.py
@@ -1,32 +1,78 @@
 from django.contrib.postgres.fields import ArrayField
+from django.core.exceptions import FieldDoesNotExist
 from django.db import models
 
 
-EXAMPLE_DATA_ARRAY_LENGTH = 701
-
-
 class Experiment(models.Model):
-    """Metadata about Experiment"""
+    """Information about each experiment"""
+    id = models.CharField(unique=True)
+    name = models.TextField()
+    description = models.TextField()
     index_stimulus_shown = models.IntegerField()
-    recording_date = models.DateField(null=True)
-    channel_region = models.CharField(30, null=True)
-    channel_id = models.CharField(30, null=True)
+
+    def __str__(self):
+        return self.experiment_name
 
 
-class Data(models.Model):
-    """Model of a single recording in an experiment"""
+class Site(models.Model):
+    """Site that data was recorded at"""
     experiment = models.ForeignKey(
         Experiment,
         on_delete=models.CASCADE
     )
-    label = models.CharField(30)
-    region_id = models.IntegerField()
-    data = ArrayField(models.DecimalField(), size=EXAMPLE_DATA_ARRAY_LENGTH)
+    id = models.CharField(30)
+    information_variable = models.CharField(30)
+    information_value = models.CharField(30)
 
-    objects = models.Manager()
+    def __str__(self):
+        return self.experiment.id + '-' + self.id
 
-    def get_stimuli(self, input_label):
-        return self.objects.filter(label=input_label)
+
+class Metadata(models.Model):
+    """Metadata about a site recorded from during an experiment"""
+    site = models.ForeignKey(
+        Site,
+        on_delete=models.CASCADE
+    )
+    information_variable = models.CharField(30)
+    information_value = models.CharField(30)
+
+    def __str__(self):
+        return str(self.site) + self.information_variable
+
+
+class Data(models.Model):
+    """Data recorded from a site during an experiment with labels shown"""
+    experiment = models.ForeignKey(
+        Experiment,
+        on_delete=models.CASCADE
+    )
+    site = models.ForeignKey(
+        Site,
+        on_delete=models.CASCADE
+    )
+    trial_number = models.IntegerField()
+    label_1 = models.CharField(30)
+    label_2 = models.CharField(30, null=True, blank=True)  # blank=true
+    label_3 = models.CharField(30, null=True, blank=True)  # blank=true
+    label_4 = models.CharField(30, null=True, blank=True)  # blank=true
+    data = ArrayField(models.DecimalField())
+
+    def __str__(self):
+        return str(self.site) + '-' + self.trial_number
+
+    def filter_by_label(self, experiment, input_label, label_number=1):
+        if label_number == 1:
+            stimuli = self.objects.filter(label_1=input_label)
+        elif label_number == 2:
+            stimuli = self.objects.filter(label_2=input_label)
+        elif label_number == 3:
+            stimuli = self.objects.filter(label_3=input_label)
+        elif label_number == 4:
+            stimuli = self.objects.filter(label_4=input_label)
+        else:
+            raise FieldDoesNotExist()
+        return stimuli
 
     def compute_binned_data(self, bin_size, step_size):
         pass
