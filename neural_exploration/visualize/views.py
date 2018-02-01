15e93b920158a397486bda49867d595fce4e6dd9
brookefitzgerald <brooke.anne.fitzgerald@gmail.com>
2018-01-31 23:16:57 -0500

    got base data passing working

--- neural_exploration/visualize/views.py
+++ neural_exploration/visualize/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
 from django.apps import apps
+from django.core.serializers.json import DjangoJSONEncoder
+from django.shortcuts import render
 
-from rest_framework.renderers import JSONRenderer
+import ipdb
+import json
+from rest_framework import renderers, viewsets
 
 from .serializers import DataSerializer
 
@@ -15,7 +18,7 @@ def SpikeDataView(request):
         apps.get_model("visualize", "Data")
         .objects
         .filter(experiment=zhang_experiment))[0]
-
+    
     serializer = DataSerializer(zhang_data)
-
-    return render(request, "visualize/spike.html", serializer.data)
+    data = renderers.JSONRenderer().render(serializer.data)
+    return render(request, "visualize/spike.html", {"experiment": data})
