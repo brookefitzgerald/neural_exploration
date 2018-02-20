e8589b60044bde5f267c1d922902ae3f77d202c5
brookefitzgerald <brooke.anne.fitzgerald@gmail.com>
2018-02-19 23:11:04 -0500

    used graph-scroll to highlight website sections and call functions on scroll

--- neural_exploration/static/js/project.js
+++ neural_exploration/static/js/project.js
@@ -1,6 +1,24 @@
 $('.form-group').removeClass('row');
 
+var section_animations = {
+	0: brain_pulse,
+	1: test_function1,
+	2: test_function2
+}
 d3.graphScroll()
-.sections(d3.selectAll('#sections > div'))
-.on('active', function(i){
-	console.log(i + 'th section active') });
+	.graph(d3.selectAll('#graph'))
+	.container(d3.select('#container'))
+  .sections(d3.selectAll('#sections > div'))
+  .on('active', function(i){ section_animations[i](); })
+
+var brain_pulse = function(){
+	console.log('brain_pulse');
+}
+
+var test_function1 = function(){
+	console.log('testing 1');
+}
+
+var test_function2 = function(){
+	console.log('testing 2');
+}
