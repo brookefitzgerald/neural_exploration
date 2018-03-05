3f9422458e0888d978bfa9a68dc81aa4bc81bf52
Brooke Fitzgerald <brooke.fitzgerald@shopify.com>
2018-03-05 17:25:59 -0500

    Updating API and Serializing Data (#4)
    * added bin data model, changed data to raster data
    
    * Removing data model, serializing bn model
    
    * Added API for specific bin sizes

--- neural_exploration/visualize/serializers.py
+++ neural_exploration/visualize/serializers.py
@@ -1,9 +1,11 @@
 from django.apps import apps
 
 from rest_framework import serializers
+import ipdb
 
 Experiment = apps.get_model("visualize", "Experiment")
-Data = apps.get_model("visualize", "Data")
+Site = apps.get_model("visualize", "Site")
+BinnedData = apps.get_model("visualize", "BinnedData")
 
 
 class ExperimentSerializer(serializers.ModelSerializer):
@@ -12,12 +14,36 @@ class ExperimentSerializer(serializers.ModelSerializer):
         model = Experiment
         fields = '__all__'
 
+class InnerListField(serializers.ListField):
+    child = serializers.DecimalField(max_digits=15, decimal_places=10)
 
-class DataSerializer(serializers.ModelSerializer):
+
+class CharListField(serializers.ListField):
+    child = serializers.CharField()
+
+
+class DataSerializer(serializers.Serializer):
     """JSON serialized representation of the Data Model"""
     experiment = ExperimentSerializer(many=False, read_only=True)
+    slug = serializers.CharField()
+    labels_one = CharListField()
+    labels_two = CharListField()
+    labels_three = CharListField()
+    labels_four = CharListField()
+    data = serializers.ListField(child=InnerListField())
 
-    class Meta:
-        model = Data
-        fields = ('data', 'trial_number', 'label_one', 'label_two',
-                  'label_three', 'label_four', 'experiment')
+
+class FirstBinSerializer(serializers.Serializer):
+    """JSON representation of the Binned Data"""
+    bin_150_50 = serializers.ListField(child=InnerListField())
+
+
+class SecondBinSerializer(serializers.Serializer):
+    """JSON representation of the Binned Data"""
+    bin_100_30=serializers.ListField(child=InnerListField())
+
+
+class ThirdBinSerializer(serializers.Serializer):
+    """JSON representation of the Binned Data"""
+    bin_50_15=serializers.ListField(child=InnerListField())
+
--- neural_exploration/visualize/views.py
+++ neural_exploration/visualize/views.py
@@ -1,14 +1,11 @@
 from django.apps import apps
-from django.core.serializers.json import DjangoJSONEncoder
 from django.shortcuts import render
 
-
-import ipdb
-import json
-from rest_framework import renderers, viewsets
+from rest_framework import renderers, viewsets, status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from .serializers import DataSerializer
+
+from .serializers import DataSerializer, FirstBinSerializer, SecondBinSerializer, ThirdBinSerializer
 
 
 def SpikeDataView(request):
@@ -16,14 +13,15 @@ def SpikeDataView(request):
         apps.get_model("visualize", "Experiment")
         .objects
         .filter(slug='zhang'))[0]
+
     zhang_data = (
-        apps.get_model("visualize", "Data")
+        apps.get_model("visualize", "Site")
         .objects
-        .filter(experiment=zhang_experiment))[0:10]
-    
-    serializer = DataSerializer(zhang_data, many=True)
+        .filter(experiment=zhang_experiment)
+        .first())
+    serializer = DataSerializer(zhang_data, many=False)
     data = renderers.JSONRenderer().render(serializer.data)
-    return render(request, "visualize/spike.html", {"experiment": data})
+    return render(request, "visualize/spike.html", {"data": data})
 
 
 @api_view(['GET'])
@@ -31,21 +29,21 @@ def data_list(request):
     if request.method == 'GET':
         data = (
             apps
-            .get_model("visualize", "Data")
+            .get_model("visualize", "Site")
             .objects
             .all()
             )
         serializer = DataSerializer(data, many=True)
-        return Response(serializer.data)
+        json_data = renderers.JSONRenderer().render(serializer.data)
+        return Response(json_data)
+
 
 @api_view(['GET'])
 def data_detail(request, pk):
-    """
-    Retrieve, update or delete a code snippet.
-    """
+    """ get data from specific site """
     try:
         data = (apps
-            .get_model("visualize", "Data")
+            .get_model("visualize", "Site")
             .objects
             .get(pk=pk))
     except data.DoesNotExist:
@@ -53,4 +51,54 @@ def data_detail(request, pk):
 
     if request.method == 'GET':
         serializer = DataSerializer(data)
-        return Response(serializer.data)
+        json_data = renderers.JSONRenderer().render(serializer.data)
+        return Response(json_data)
+
+
+def get_queryset_of_bin_size(i):
+    bin_size_dict = {
+        "1": "bin_150_50",
+        "2": "bin_100_30",
+        "3": "bin_50_15",
+        }
+    try: 
+        return (apps
+                .get_model("visualize", "BinnedData")
+                .objects
+                .all()
+                .only(bin_size_dict[i]))
+    except:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+def binned_filter_detail(request, i, pk):
+    """ Get specific data with specific bin size"""
+    bin_serializer_dict = {
+        "1":  FirstBinSerializer,
+        "2": SecondBinSerializer,
+        "3": ThirdBinSerializer}
+    try:
+        data = (
+            get_queryset_of_bin_size(i)
+            .get(pk=pk))
+        if request.method == 'GET':
+            serializer = bin_serializer_dict[i](data)
+            json_data = renderers.JSONRenderer().render(serializer.data)
+            return Response(json_data)
+    except data.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+
+@api_view(['GET'])
+def binned_filter_list(request, i):
+    """ Get all data with specific bin size"""
+    bin_serializer_dict = {
+        "1":  FirstBinSerializer,
+        "2": SecondBinSerializer,
+        "3": ThirdBinSerializer}
+    if request.method == 'GET':
+        data = get_queryset_of_bin_size(i)
+        serializer = bin_serializer_dict[i](data, many=True)
+        json_data = renderers.JSONRenderer().render(serializer.data)
+        return Response(json_data)
